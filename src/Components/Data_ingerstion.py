31c64abb6146ff405468b50fcb1258b6b9e1d8dc
Shreyas9876 <113093819+Shreyas9876@users.noreply.github.com>
2025-04-26 07:24:21 +0000

    Data-Ingestion

--- src/Components/Data_ingerstion.py
+++ src/Components/Data_ingerstion.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from src.Exception import CustomException
-from src.logger import logging
+from src.Logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
@@ -25,7 +25,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv(r'notebook\data\stocks.csv')
+            df=pd.read_csv(r'Notebook/Data/stocks.csv')  
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -39,7 +39,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
+            logging.info("Ingestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
