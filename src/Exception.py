e38397aad0942d75dfdffe44c12b1fb0ff9f8a26
Shreyas9876 <113093819+Shreyas9876@users.noreply.github.com>
2025-05-18 08:21:41 +0000

    Updated multiple components and added utils.py

--- src/Exception.py
+++ src/Exception.py
@@ -1,22 +1,29 @@
 import sys
 from src.Logger import logging
+from typing import Any
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error))
+def error_message_detail(error: Any, error_detail: sys) -> str:
+    _, _, exc_tb = error_detail.exc_info()
+    
+    if exc_tb is None:
+        file_name = "Unknown"
+        line_number = "Unknown"
+    else:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+
+    error_message = (
+        f"Error occurred in Python script [{file_name}] "
+        f"at line number [{line_number}] "
+        f"with error message [{str(error)}]"
+    )
 
     return error_message
 
-    
-
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self, error_message: Any, error_detail: sys) -> None:
         super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
-    
-    def __str__(self):
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self) -> str:
         return self.error_message
-
-
--- src/Logger.py
+++ src/Logger.py
@@ -2,19 +2,29 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs")
-os.makedirs(logs_path,exist_ok=True)
+# Setting up logs directory and log file
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+logs_path = os.path.join(BASE_DIR, "logs")
+os.makedirs(logs_path, exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+# Setting up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
+# File Handler
+file_handler = logging.FileHandler(LOG_FILE_PATH)
+file_handler.setFormatter(logging.Formatter("[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s"))
 
-)
+# Console Handler
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(logging.Formatter("[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s"))
+
+# Adding Handlers
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 if __name__ == "__main__":
-    logging.info("logging as started")
+    logging.info("Logging has started")
